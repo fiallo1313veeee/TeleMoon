032c2b28c1209daf0811eb3c1eb4f4ed30cbc4c6
fiallo1313veeee <aliarjmandi339@yahoo.com>
2016-05-21 13:11:12 -0700

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = { 196560568,136378403,tonumber(our_id)},--Sudo users
+    sudo_users = { 196560568,136378403,167288719,tonumber(our_id)},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
